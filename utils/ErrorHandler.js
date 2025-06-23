375df04e6ec4f54bcb55c06471132c9e42d252b8
ramses-ayala <ramses0595@gmail.com>
2025-06-22 21:32:09 -0600

    fixing some details about error catching

--- utils/ErrorHandler.js
+++ utils/ErrorHandler.js
@@ -3,12 +3,9 @@ import * as yup from 'yup'
 export class ErrorHandler {
 
     static formatError(error) {
-
         if (!Array.isArray(error) && typeof(error) === 'object' && error instanceof yup.ValidationError) {
             console.log('ENTRO EN EL IF, error: ', error)
-            return { data: { message: null }, success: false, httpCode: 422, errors: error.errors }
-        } else {
-            console.log('NO es un error de validación !!!')
+            return { data: { message: null, errors: error.errors } }
         }
     }
 }
